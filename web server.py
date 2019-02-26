107e158e90c0128efdf38b09482b093b30a6a395
pdswift <38026357+pdswift@users.noreply.github.com>
2019-02-26 07:32:28 -0800

    Add files via upload

--- web server.py
+++ web server.py
@@ -1,11 +1,12 @@
 #This script updates the zip code database
 #CNA 335, winter 19
 # Parker Swift, pdswift@student.rtc.edu
+#base code from https://daanlenaerts.com/blog/2015/06/03/create-a-simple-http-server-with-python-3/
 # !/usr/bin/env python
 
 import mysql.connector
-#connects to my
-conn = mysql.connecor.connect(user='root', password='', host='127.0.0.1', database='zip code data')
+#connects to WAMP on localhost
+conn = mysql.connector.connect(user='root', password='', host='127.0.0.1', database='zip code data')
 c = conn.cursor()
 
 print('connected to zip_code_data database')
@@ -24,21 +25,19 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
         # Send message back to client
-        message = "This web page serches and updates the zip code database"
+        message = "This web page serches the zip code database"
         # Write content as utf-8 data
         self.wfile.write(bytes(message, "utf8"))
         return
 
-
+ # starts a web server on port 5000
 def run():
     print('starting server...')
 
     # Server setting
-    # starts a web server on port 5000
-    server_address = ('172.16.0.27', 5000)
+    server_address = ('127.0.0.1', 5000)
     httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
     print('running server...')
     httpd.serve_forever()
-
 
 run()
